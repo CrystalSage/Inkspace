a2ed15cb3229869f63f6f0e0be956c368dbadade
Bourbon <51860725+CrystalSage@users.noreply.github.com>
2022-05-26 11:21:37 +0530

    Add templates

---
+++ README.md
@@ -0,0 +1,6 @@
+# Inkspace
+This is a framework for blogs, built in Rust. 
+
+# Running the blog
+Currently in test mode. Use `cargo run` and it should start a server at 
+some port.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use rocket_dyn_templates::Template;
+use rocket_dyn_templates::context;
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context!{
+        name: "Bourbon",
+        quote: "The weak links of strong chain"
+    })
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
 }
