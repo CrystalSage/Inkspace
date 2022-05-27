c41e288b5f36e1637ebf8aa495279d01be414d51
Bourbon <51860725+CrystalSage@users.noreply.github.com>
2022-05-28 00:22:45 +0530

    Add lib file for utility functions

---
+++ lib.rs
@@ -0,0 +1,3 @@
+pub fn add_page(build: Building){
+    let file = read_bytes()
+}
--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,7 @@ fn index() -> Template {
     })
 }
 
-
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![index])
-        .attach(Template::fairing())
+    dbg!(rocket::build())
 }
