1b3ae36bef1692eb5ed6ee4abb144932ff05b83f
Amit <amitkulkarni2028@gmail.com>
2022-06-13 20:57:40 +0530

    Accept templates through commandline

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 #[macro_use] extern crate rocket;
+
 use rocket_dyn_templates::Template;
 use rocket_dyn_templates::context;
 
+use std::env;
+
+fn getfile() -> String{
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    return filename.to_string();
+}
+
 #[get("/")]
 fn index() -> Template {
-    Template::render("index", context!{
+    let fname: String = getfile();
+    Template::render(fname , context!{
         name: "Bourbon",
-        quote: "The weak links of strong chain",
     })
 }
 
 #[launch]
 fn rocket() -> _ {
+
     rocket::build()
         .mount("/", routes![index])
         .attach(Template::fairing())
